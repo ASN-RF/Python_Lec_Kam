d0cc24bd5bba4d28ae88326c76b76b693c777d1f
Gr_n RF <gr.nrfinfo@gmail.com>
2023-02-14 08:21:31 +0600

    Added new info in 3_lec

--- 3_lec/Task_1.py
+++ 3_lec/Task_1.py
@@ -37,14 +37,77 @@
 # print (out)
 
 # --------- ВАРИАНТ СЕРГЕЯ 2 -----------------
-def select (f, col):
-    return [f(x) for x in col]
+# def select (f, col):
+#     return [f(x) for x in col]
 
-def where (f, col):
-    return [x for x in col if f(x)]
+# def where (f, col):
+#     return [x for x in col if f(x)]
 
-data = '1 2 3 5 8 15 23 38'.split()
-res = select(int, data)
-res = where(lambda x: not x % 2, res)
-res = select(lambda x: (x, x**2), res)
-print(res)
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = select(int, data)
+# res = where(lambda x: not x % 2, res)
+# res = select(lambda x: (x, x**2), res)
+# print(res)
+# ---- АНАЛОГО с MAP -----
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = where(lambda x: not x % 2, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+# ---- АНАЛОГО с MAP и FILTER -----
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = filter(lambda x: not x % 2, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+# --------- MAP  ------
+
+# li = [x for x in range(1, 20)]
+# print (li)
+# li = list(map(lambda x: x+10, li))
+# print (li)
+
+# Задача 3
+# data = list (map(int, input().split()))
+# print (data)
+
+# аналог
+# data = map(int, input().split())
+# for e in data:
+#     print (e)
+
+# data = map(int, '1 2 3'.split())  # буз указания list, можно пройти только один раз пожтому надо добавлять list
+# for e in data:
+#     print (e)
+
+# print ('--')
+# for e in data:
+#     print(e)
+
+
+# ----- FILTER -----
+# data = [x for x in range(10)]
+# res = list (filter(lambda x: not x % 2, data))
+# print(res)
+
+
+# --------- ZIP  ------
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]   # функция zip будет выводить значения по размеру наименьшей переменной
+# data = list(zip(users, ids, salary))
+# print(data)
+
+# --------- ZIP  ------
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]  
+data = list(enumerate(users))
+print(data)
